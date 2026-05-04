e3cd11249eea7594ed950e2afef1f5499833b478
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-05-05 03:14:02 +1000

    Add logging in drop for ClientHandle

--- src/blocking/client.rs
+++ src/blocking/client.rs
@@ -1427,8 +1427,8 @@ impl Drop for InnerClientHandle {
                 trace!("closing runtime thread ({id:?})");
                 self.tx.take();
                 trace!("signaled close for runtime thread ({id:?})");
-                thread.join();
-                trace!("closed runtime thread ({id:?})");
+                let res = thread.join();
+                trace!("closed runtime thread ({id:?}): {res:?}");
             }
             TokioTask { handle, task } => {
                 let id = task.id();
@@ -1438,6 +1438,7 @@ impl Drop for InnerClientHandle {
                 trace!("signaled close for runtime task ({id})");
 
                 handle.block_on(task);
+                trace!("closed runtime task ({id}): {res:?}");
             }
         }
     }
